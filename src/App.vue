d3f2170130ca9ba9e8262cab8ec01bf70434c35d
MeenaAlagiah <117711147+MeenaAlagiah@users.noreply.github.com>
2023-08-30 17:44:38 +0530

    Committing the Vue RTE Getting Started sample

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script>
+import { RichTextEditorComponent, Toolbar, Image, HtmlEditor, Link, Table, QuickToolbar } from '@syncfusion/ej2-vue-richtexteditor';
+export default {
+    name: "App",
+    components: {
+      "ejs-richtexteditor" : RichTextEditorComponent         
+    },
+    methods: {
+      click: function()
+      {
+        debugger;
+        const content = this.$refs.rteInstance.getHtml();
+        console.log(content);
+      }
+    },
+    data: function()
+    {
+      return{
+        toolbarSettings: {
+          items: [ 'Bold', 'Italic', 'Undo', 'Redo', 'CreateTable', 'Image', 'CreateLink']
+        }
+      }
+    },
+    provide:{
+      richtexteditor: [Toolbar, Image, HtmlEditor, Link, Table, QuickToolbar ]
+    }
+  }
+
+</script>
+
+<template>
+  
+<ejs-richtexteditor ref="rteInstance" height="200px" width="700px" :toolbarSettings="toolbarSettings">
+  <img src="https://ej2.syncfusion.com/demos/src/rich-text-editor/images/RTEImage-Feather.png"
+       alt="Logo" style="height:150px; width:150px;">
+   <p>
+    <a href="https://ej2.syncfusion.com/vue/documentation/rich-text-editor/link">Rich Text Editor</a> 
+      component is a WYSIWYG ("what you see is what you get") editor that provides the best user experience to create and update the content. Users can format their content using standard toolbar commands.
+   </p>
+</ejs-richtexteditor>
+</template>
+
+<style>
+@import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-lists/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-vue-richtexteditor/styles/material.css";
+</style>
